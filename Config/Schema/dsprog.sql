5cd1288fdc9bd5ee261f830b1a0823f34fbfd62c
Daniel Henrique dos Santos Oliveira <dsprog@gmail.com>
2013-05-22 20:16:11 -0300

    Update dsprog.sql

--- Config/Schema/dsprog.sql
+++ Config/Schema/dsprog.sql
@@ -3,18 +3,107 @@
 -- Server version:               5.5.27 - MySQL Community Server (GPL)
 -- Server OS:                    Win32
 -- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2013-05-16 22:58:33
+-- Date/time:                    2013-05-22 20:09:44
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!40014 SET FOREIGN_KEY_CHECKS=0 */;
--- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping structure for table dsprog.contents
+DROP TABLE IF EXISTS `contents`;
+CREATE TABLE IF NOT EXISTS `contents` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(20) DEFAULT NULL,
+  `user_id` int(20) NOT NULL DEFAULT '0',
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `body` text COLLATE utf8_unicode_ci NOT NULL,
+  `excerpt` text COLLATE utf8_unicode_ci,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `promote` tinyint(1) NOT NULL DEFAULT '0',
+  `type` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'pagina',
+  `image` varchar(255) COLLATE utf8_unicode_ci DEFAULT '0',
+  `image_dir` varchar(255) COLLATE utf8_unicode_ci DEFAULT '0',
+  `updated` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table dsprog.contents: ~2 rows (approximately)
+/*!40000 ALTER TABLE `contents` DISABLE KEYS */;
+INSERT INTO `contents` (`id`, `parent_id`, `user_id`, `title`, `slug`, `body`, `excerpt`, `status`, `promote`, `type`, `image`, `image_dir`, `updated`, `created`) VALUES
+  (1, NULL, 1, 'Teste', 'teste', 'Corpo do site', 'Resumo', 1, 1, 'portfolio', '0', '0', '2013-05-22 02:25:36', '2013-05-22 02:25:36'),
+	(2, NULL, 1, 'dasdsa', 'dasdsd', 'dasdas', '', 0, 1, 'image/jpeg', 'Hydrangeas.jpg', '', '2013-05-22 02:58:11', '2013-05-22 02:58:11');
+/*!40000 ALTER TABLE `contents` ENABLE KEYS */;
 
--- Data exporting was unselected.
+
+-- Dumping structure for table dsprog.links
+DROP TABLE IF EXISTS `links`;
+CREATE TABLE IF NOT EXISTS `links` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(20) DEFAULT NULL,
+  `menu_id` int(20) NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `class` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `link` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `target` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `rel` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `lft` int(11) DEFAULT NULL,
+  `rght` int(11) DEFAULT NULL,
+  `visibility_roles` text COLLATE utf8_unicode_ci,
+  `params` text COLLATE utf8_unicode_ci,
+  `updated` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table dsprog.links: ~0 rows (approximately)
+/*!40000 ALTER TABLE `links` DISABLE KEYS */;
+/*!40000 ALTER TABLE `links` ENABLE KEYS */;
+
+
+-- Dumping structure for table dsprog.menus
+DROP TABLE IF EXISTS `menus`;
+CREATE TABLE IF NOT EXISTS `menus` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `alias` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `weight` int(11) DEFAULT NULL,
+  `link_count` int(11) NOT NULL,
+  `params` text COLLATE utf8_unicode_ci,
+  `updated` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `alias` (`alias`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table dsprog.menus: ~0 rows (approximately)
+/*!40000 ALTER TABLE `menus` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menus` ENABLE KEYS */;
+
+
+-- Dumping structure for table dsprog.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table dsprog.users: ~1 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `nome`, `email`, `username`, `password`, `created`, `modified`) VALUES
+	(1, 'Daniel Santos Oliveira', 'dsprog@gmail.com', 'dsprog', 'luanda', '2013-05-21 01:59:16', '2013-05-21 02:12:42');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40014 SET FOREIGN_KEY_CHECKS=1 */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
